2eb34ccf5d8b6cb8ec4f9bf48bf19e8989d890c1
moxuy <jichangee@gmail.com>
2022-06-08 21:27:00 +0800

    添加别名

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { ArcoResolver } from 'unplugin-vue-components/resolvers';
@@ -6,6 +7,11 @@ import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src')
+    }
+  },
   plugins: [vue(),
     Components({
       resolvers: [
