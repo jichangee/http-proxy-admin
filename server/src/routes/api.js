0782d60ebc686debdc829af369af0c130a963d81
moxuy <jichangee@gmail.com>
2022-06-10 11:14:46 +0800

    添加put接口

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -19,6 +19,22 @@ api.delete('/:id', async (req, res) => {
   res.status(200).json(proxyConfig.list)
 })
 
+api.put('/:id', async (req, res) => {
+  const body = req.body
+  const proxyConfig = getProxyConfig()
+  const index = proxyConfig.list.findIndex(item => `${item.id}` === req.params.id)
+  if (index === -1) {
+    return res.status(400).json({ message: 'id is not found' })
+  }
+  const updatedItem = {
+    ...proxyConfig.list[index],
+    ...body
+  }
+  proxyConfig.list.splice(index, 1, updatedItem)
+  setProxyConfig(proxyConfig)
+  res.status(200).json(updatedItem)
+})
+
 api.post('/reset', async (req, res) => {
   setProxyConfig()
   res.status(200).json(defaultProxyConfig.list)
